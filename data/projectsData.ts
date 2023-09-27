fc66af98a3cd66d0c47703f3d9633a17be153cc0
deinal1989 <deinal1989@gmail.com>
2023-09-27 14:24:04 -0500

    Actualización: Archivo projectsData.ts'

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,28 +2,28 @@ const projectsData = [
   {
     title: 'Desarrollo de software y Apps',
     description: `En DeiVal.online, estamos comprometidos en brindar soluciones tecnológicas de vanguardia para ayudarte a alcanzar tus objetivos comerciales.`,
-    imgSrc: '/public/static/images/servicios/1.jpg',
+    imgSrc: '/static/images/servicios/1.jpg',
     imgAlt: 'Desarrollo de software y Apps', // Agrega una descripción alt para la imagen
     href: '/blog/services/desarrollos',
   },
   {
     title: 'Consultoría en Planes de Compensación para Empresas de Mercadeo en Red',
     description: `Potencia tu empresa de mercadeo en red con un plan de compensación a medida. Nuestra consultoría te brinda la clave del éxito.`,
-    imgSrc: '/public/static/images/servicios/2.jpg',
+    imgSrc: '/static/images/servicios/2.jpg',
     imgAlt: 'Consultoría en Planes de Compensación para Empresas de Mercadeo en Red', // Agrega una descripción alt para la imagen
     href: '/blog/services/consultorias', 
   },
   {
     title: 'Mentoría para Emprendedores en Desarrollo de Proyectos Digitales',
     description: `¿Eres un emprendedor con un proyecto digital en mente? Únete a nuestra mentoría y convierte tu visión en una realidad exitosa en el mundo digital.`,
-    imgSrc: '/public/static/images/servicios/3.jpg',
+    imgSrc: '/static/images/servicios/3.jpg',
     imgAlt: 'Mentoría para Emprendedores en Desarrollo de Proyectos Digitales', // Agrega una descripción alt para la imagen
     href: '/blog/services/mentorias',
   },
   {
     title: 'Desarrollo de Imagen Corporativa',
     description: `Construir una imagen corporativa sólida es esencial en el mundo empresarial actual. Nuestros servicios de diseño de logotipos, selección de colores y tipografía, y desarrollo de sitios web pueden ayudar a su empresa a destacar y conectarse con su audiencia.`,
-    imgSrc: '/public/static/images/servicios/4.jpg',
+    imgSrc: '/static/images/servicios/4.jpg',
     imgAlt: 'Desarrollo de Imagen Corporativa', // Agrega una descripción alt para la imagen
     href: '/blog/services/iamgencorporativa',
   },
