ed20ad82c0020c14f472039d15a90bfc14c67193
deinal1989 <deinal1989@gmail.com>
2023-09-27 21:12:35 -0500

    Actualización de rutas de imágenes

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: 'Desarrollo de software y Apps',
     description: `En DeiVal.online, estamos comprometidos en brindar soluciones tecnológicas de vanguardia para ayudarte a alcanzar tus objetivos comerciales.`,
-    imgSrc: '/static/images/servicios/2.jpg',
+    imgSrc: '/static/images/2.jpg',
     imgAlt: 'Desarrollo de software y Apps', // Agrega una descripción alt para la imagen
     href: '/blog/services/desarrollos',
   },
@@ -16,14 +16,14 @@ const projectsData = [
   {
     title: 'Mentoría para Emprendedores en Desarrollo de Proyectos Digitales',
     description: `¿Eres un emprendedor con un proyecto digital en mente? Únete a nuestra mentoría y convierte tu visión en una realidad exitosa en el mundo digital.`,
-    imgSrc: '/static/images/servicios/3.jpg',
+    imgSrc: '/static/images/3.jpg',
     imgAlt: 'Mentoría para Emprendedores en Desarrollo de Proyectos Digitales', // Agrega una descripción alt para la imagen
     href: '/blog/services/mentorias',
   },
   {
     title: 'Desarrollo de Imagen Corporativa',
     description: `Construir una imagen corporativa sólida es esencial en el mundo empresarial actual. Nuestros servicios de diseño de logotipos, selección de colores y tipografía, y desarrollo de sitios web pueden ayudar a su empresa a destacar y conectarse con su audiencia.`,
-    imgSrc: '/static/images/servicios/4.jpg',
+    imgSrc: '/static/images/4.jpg',
     imgAlt: 'Desarrollo de Imagen Corporativa', // Agrega una descripción alt para la imagen
     href: '/blog/services/iamgencorporativa',
   },
